f433ac69bcbcbb4bff80f7233a2e1a0d85de3a83
Jumana <13jumanakp@gmail.com>
2026-06-13 19:45:28 +0400

    updated fetch

--- src/app/services/products-services.ts
+++ src/app/services/products-services.ts
@@ -1,23 +1,37 @@
-import { ServiceBase } from "./service-base"
+import { ServiceBase } from "./service-base";
 
-export class ProductsService extends ServiceBase{
-    static getProducts = async () => {
-        var productsResp = await fetch (this.getUrl(`/products`),{
-        cache:'no-store'
-        }
+export class ProductsService extends ServiceBase {
+  static async getProducts() {
+    const response = await fetch(
+      this.getUrl("/products"),
+      {
+        cache: "no-store",
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Products API Error: ${response.status}`
       );
-         
-
-  var products = await productsResp.json();
-  return products;
-
     }
 
-     static getProductById =async (id:number) => {
-       var productResp = await fetch (this.getUrl(`/products/` + id));
-  var product = await productResp.json();
-  return product;
+    return response.json();
+  }
 
-     }
+  static async getProductById(id: number) {
+    const response = await fetch(
+      this.getUrl(`/products/${id}`),
+      {
+        cache: "no-store",
+      }
+    );
 
+    if (!response.ok) {
+      throw new Error(
+        `Product API Error: ${response.status}`
+      );
+    }
+
+    return response.json();
+  }
 }
